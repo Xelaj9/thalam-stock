db076b6f2797401fe0293d90f50081e5a0649e8f
Xela9 <98663589+Xelaj9@users.noreply.github.com>
2026-04-08 06:54:20 +0700

    fix supabase key

--- src/supabase.js
+++ src/supabase.js
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://ogmrzbtxylazkffnbvyk.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9nbXJ6YnR4eWxhemtmZm5idnlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk4NjkwODEsImV4cCI6MjA2NTQ0NTA4MX0.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9nbXJ6YnR4eWxhemtmZm5idnlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1ODI1MjQsImV4cCI6MjA5MTE1ODUyNH0.ZZdxM-dYXBEIIu9_TXBqXkQcM_cGF2tEzji-X0A5ukg'
 )
