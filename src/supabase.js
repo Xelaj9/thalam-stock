eb767da49e0a17e602a8311c064709e3aa4d4268
Xela9 <98663589+Xelaj9@users.noreply.github.com>
2026-04-08 06:44:54 +0700

    fix supabase env

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://supabase.com/dashboard/project/ogmrzbtxylazkffnbvyk/settings/api-keys'
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9nbXJ6YnR4eWxhemtmZm5idnlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1ODI1MjQsImV4cCI6MjA5MTE1ODUyNH0.ZZdxM-dYXBEIIu9_TXBqXkQcM_cGF2tEzji-X0A5ukg'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
+)
