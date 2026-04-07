73f49b090999bd9e5b00c4f116fae9bd89d9a2de
Xela9 <98663589+Xelaj9@users.noreply.github.com>
2026-04-08 06:49:11 +0700

    fix supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_KEY
+  'https://ogmrzbtxylazkffnbvyk.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9nbXJ6YnR4eWxhemtmZm5idnlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk4NjkwODEsImV4cCI6MjA2NTQ0NTA4MX0.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
 )
